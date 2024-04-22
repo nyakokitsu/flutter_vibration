91730ce61206dd41b63aae62cf6788d1b06f93ba
nyakokitsu <140620899+nyakokitsu@users.noreply.github.com>
2024-04-22 21:31:30 +0300

    Vibration Logic update

--- vibration/android/src/main/java/com/benjaminabel/vibration/VibrationPlugin.java
+++ vibration/android/src/main/java/com/benjaminabel/vibration/VibrationPlugin.java
@@ -3,49 +3,66 @@ package com.benjaminabel.vibration;
 import android.content.Context;
 import android.os.Build;
 import android.os.Vibrator;
+import android.os.VibrationEffect;
 import android.os.VibratorManager;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-public class VibrationPlugin implements FlutterPlugin {
-    private static final String CHANNEL = "vibration";
+public class VibrationPlugin implements FlutterPlugin, MethodCallHandler {
     private MethodChannel methodChannel;
+    private Vibrator vibrator;
 
     @Override
-    public void onDetachedFromEngine(FlutterPluginBinding binding) {
-        teardownChannels();
-    }
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        Context context = flutterPluginBinding.getApplicationContext();
+        vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
-    @SuppressWarnings("deprecation")
-    public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-        final VibrationPlugin vibrationPlugin = new VibrationPlugin();
-
-        vibrationPlugin.setupChannels(registrar.messenger(), registrar.context());
+        methodChannel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "vibration");
+        methodChannel.setMethodCallHandler(this);
     }
 
     @Override
-    public void onAttachedToEngine(FlutterPluginBinding binding) {
-        setupChannels(binding.getBinaryMessenger(), binding.getApplicationContext());
-    }
-
-    private void setupChannels(BinaryMessenger messenger, Context context) {
-        Vibrator vibrator;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            VibratorManager vibratorManager = (VibratorManager) context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
-            vibrator = vibratorManager.getDefaultVibrator();
-        } else {
-            vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        switch (call.method) {
+            case "vibrate":
+                long duration = (long) call.arguments;
+                vibrate(duration);
+                result.success(null);
+                break;
+            default:
+                result.notImplemented();
+                break;
         }
-        final VibrationMethodChannelHandler methodChannelHandler = new VibrationMethodChannelHandler(new Vibration(vibrator));
-
-        this.methodChannel = new MethodChannel(messenger, CHANNEL);
-        this.methodChannel.setMethodCallHandler(methodChannelHandler);
     }
 
-    private void teardownChannels() {
-        this.methodChannel.setMethodCallHandler(null);
-        this.methodChannel = null;
+    private void vibrate(long duration) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            vibrator.vibrate(duration);
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null;
+    }
+
+    // For older Flutter projects using the old v1 Android embedding method
+    @SuppressWarnings("deprecation")
+    public static void registerWith(Registrar registrar) {
+        VibrationPlugin plugin = new VibrationPlugin();
+        plugin.vibrator = (Vibrator) registrar.context().getSystemService(Context.VIBRATOR_SERVICE);
+
+        MethodChannel channel = new MethodChannel(registrar.messenger(), "vibration");
+        channel.setMethodCallHandler(plugin);
     }
 }
